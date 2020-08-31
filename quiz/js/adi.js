814fd012a4406865c381918fd4308441b028d708
Aditya Kameswara Rao Nandula <adirosgaz@gmail.com>
2020-08-31 21:17:43 +0530

    New Features Added to Quiz

--- quiz/js/adi.js
+++ quiz/js/adi.js
@@ -6,7 +6,7 @@ Virtual Labs IIT Kharagpur.
 Email: akraonandula@gmail.com
 
 */
-
+var i=0,c=1;
 $(document).ready(function(){
   var i=0;
   $("#ti").show("slow");
@@ -17,24 +17,34 @@ $(document).ready(function(){
     $("#qes").show("slow");
     $("#q1").show("slow");
     $("#q1").nextAll().hide("slow");
+    $("#Qe").text(" Question "+c+" of 4 ");
+    c=c+1;
   });
   $("#q11,#q12,#q13,#q14").change(function(){
       $("#q1").hide("slow");
       $("#q2").show("slow");
+      $("#Qe").text(" Question "+c+" of 4 ");
+    c=c+1;
   });
   $("#q21,#q22,#q23,#q24").change(function(){
       $("#q2").hide("slow");
       $("#q3").show("slow");
+      $("#Qe").text(" Question "+c+" of 4 ");
+    c=c+1;
+
   });
   $("#q31,#q32,#q33,#q34").change(function(){
       $("#q3").hide("slow");
       $("#q4").show("slow");
+      $("#Qe").text(" Question "+c+" of 4 ");
+      c=c+1;
   });
   $("#q41,#q42,#q43,#q44").change(function(){
       $("#q4").hide("slow");
       //i=i+1;
       $("#rst").show("slow");
       $("#ma").text(" You got "+i+" out of 4 ");
+      alert(string($(this).val()));
   });
   $("#q11,#q22,#q31,#q43").click(function(){
   i=i+1;
@@ -42,7 +52,8 @@ $(document).ready(function(){
   $("#rst").click(function(){
     $("#rst").hide("slow");
     $("#ti").show("slow");
-    i=0
+    i=0;
+    c=1;
   location.reload(true);
   });
 });
