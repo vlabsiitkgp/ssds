4e78e8d0d1b8215c83c73db5332f920243922f57
Aditya Kameswara Rao Nandula <adirosgaz@gmail.com>
2021-07-27 23:58:29 +0530

    New developed template (under test).

---
+++ newexp2/js/adi_clpQuiz.js
@@ -0,0 +1,138 @@
+/*
+This Scrtpt file is developed by
+Aditya Kameswara Rao Nandula
+Senior Research Fellow,
+Virtual Labs IIT Kharagpur.
+LinkedIn: https://in.linkedin.com/in/akraonandula/
+
+*/
+//var i=0;
+var ql=0;
+var cl=0;
+var pql=0;
+var pcl=0;
+var day = new Date();
+var s=0;
+var m=0;
+function st() {
+var day = new Date();
+var se=day.getSeconds();
+var da= ct(day.getDate()) +"/"+ct(day.getMonth()) +"/"+day.getFullYear() +" "+ct(day.getHours()) + " : " + ct(day.getMinutes()) + " : " + ct(se);
+document.getElementById('qqd').innerHTML =da;
+if(s==59){
+  m++;
+  s=0;
+}
+else{
+s++;
+}
+ document.getElementById('qqt').innerHTML = "Time: "+ct(m)+ " : "+ ct(s) + " sec";
+
+var t = setTimeout(st, 1000);
+}
+function ct(i) {
+if (i < 10) {i = "0" + i};
+return i;
+}
+
+$(document).ready(function(){
+  // For pre Quiz
+  ql = $('.preq').length - 1;
+  cl=ql;
+  $("#qq").carousel("pause");
+  $("#peqqn").text("Question "+(ql-cl+1)+" / "+(ql+1));
+  $("#pb").hide();
+  //$("#sb").hide();
+  $("#pb").click(function(){
+    cl=cl+1;
+    $("#peqqn").text("Question "+(ql-cl+1)+" / "+(ql+1));
+    $("#qq").carousel("prev");
+    if(cl>0){
+      $("#nb").show();
+      //$("#sb").hide();
+    }
+    if(cl==ql){
+      $("#pb").hide();
+      //$("#sb").hide();
+    }
+    $("#prb").css("width",(100-cl/ql*100)+"%");
+});
+$("#nb").click(function(){
+
+  cl=cl-1;
+  $("#peqqn").text("Question "+(ql-cl+1)+" / "+(ql+1));
+  if(cl==0){
+    $("#nb").hide();
+    //$("#sb").show();
+  }
+  $("#pb").show();
+  $("#qq").carousel("next");
+  $("#prb").css("width",(100-pcl/pql*100)+"%");
+});
+
+
+    // For post Quiz
+
+    pql = $('.poq').length - 1;
+    pcl=pql;
+    $("#pqq").carousel("pause");
+    $("#pqqn").text("Question "+(pql-pcl+1)+" / "+(pql+1));
+    $("#ppb").hide();
+    //$("#sb").hide();
+    $("#ppb").click(function(){
+      pcl=pcl+1;
+      $("#pqqn").text("Question "+(pql-pcl+1)+" / "+(pql+1));
+      $("#pqq").carousel("prev");
+      if(pcl>0){
+        $("#pnb").show();
+        //$("#sb").hide();
+      }
+      if(pcl==pql){
+        $("#ppb").hide();
+        //$("#sb").hide();
+      }
+      $("#pprb").css("width",(100-pcl/pql*100)+"%");
+
+
+  });
+  $("#pnb").click(function(){
+
+    pcl=pcl-1;
+    $("#pqqn").text("Question "+(pql-pcl+1)+" / "+(pql+1));
+    if(pcl==0){
+      $("#pnb").hide();
+      //$("#sb").show();
+    }
+    $("#ppb").show();
+    $("#pqq").carousel("next");
+    $("#pprb").css("width",(100-pcl/pql*100)+"%");
+  });
+});
+
+
+$("#q1c4,#q2c1,#q3c1").click(function(){
+//i=i+1;
+$(".an"+this.value).css("background","whitesmoke");
+$("#o"+this.id).css("background","#a1e890");
+//$("#qqsc").text("Score : "+i+" / "+(ql+1));
+});
+$("#q1c1,#q1c2,#q1c3,#q2c2,#q2c3,#q2c4,#q3c2").click(function(){
+//i=i+1;
+$(".an"+this.value).css("background","whitesmoke");
+$("#o"+this.id).css("background","#dc8c8c");
+//$("#qqsc").text("Score : "+i+" / "+(ql+1));
+});
+
+
+$("#pq1c1,#pq2c2,#pq3c4").click(function(){
+//i=i+1;
+$(".pan"+this.value).css("background","whitesmoke");
+$("#o"+this.id).css("background","#a1e890");
+//$("#qqsc").text("Score : "+i+" / "+(ql+1));
+});
+$("#pq1c2,#pq1c3,#pq1c4,#pq2c1,#pq2c3,#pq2c4,#pq3c1,#pq3c2,#pq3c3").click(function(){
+//i=i+1;
+$(".pan"+this.value).css("background","whitesmoke");
+$("#o"+this.id).css("background","#dc8c8c");
+//$("#qqsc").text("Score : "+i+" / "+(ql+1));
+});
---
+++ newexp2/js/clamp.js
@@ -0,0 +1,366 @@
+/*
+This Scrtpt file is developed by
+Aditya Kameswara Rao Nandula
+Senior Research Fellow,
+Virtual Labs IIT Kharagpur.
+Email: akraonandula@gmail.com
+
+*/
+  var ckt=0;
+  var ch=0;
+  var cla=0;
+  var amp= $("#av").val();
+  var fre= $("#fv").val();
+  var vr= $("#vrv").val();
+  var vr1= $("#v1v").val();
+  var vr2= $("#v2v").val();
+
+  var vc1= $("#vc1v").val();
+  var t1= $("#t1v").val();
+  var px1= $("#px1v").val();
+  var py1= $("#py1v").val();
+  var vc2= $("#vc2v").val();
+  var t2= $("#t2v").val();
+  var px2= $("#px2v").val();
+  var py2= $("#py2v").val();
+
+  function rng(start,step,end) {
+      var ans = [];
+      for (let i = start; i <= end; i=i+step) {
+          ans.push(i);
+      }
+      return ans;
+  };
+  function rngvr(vr,vi) {
+              var ans = [];
+              for(let i=0;vi[i]!=undefined;i++)
+                  {
+                        ans.push(vr);
+                      }
+              return ans;
+    };
+  function rngscn(a,f,x) {
+      var ans = [];
+        for(let i=0;x[i]!=undefined;i++)
+          {
+            ans.push(a*Math.sin(f*x[i]));
+          }
+      return ans;
+  };
+    function po(v,x) {
+                var ans = [];
+                for(let i=0;v[i]!=undefined;i++)
+                    {
+                          ans.push(v[i]+Number(x));
+                        }
+                return ans;
+      };
+  function plt(cha){
+    var amp= $("#av").val();
+    var fre= $("#fv").val();
+    var vr= $("#vrv").val();
+    var vc1= $("#vc1v").val();
+    var t1= $("#t1v").val();
+    var px1= $("#px1v").val();
+    var py1= $("#py1v").val();
+    var vc2= $("#vc2v").val();
+    var t2= $("#t2v").val();
+    var px2= $("#px2v").val();
+    var py2= $("#py2v").val();
+                var amp1=amp/vc1;
+                var amp2=amp/vc2;
+                fre1=fre*t1;
+                fre2=fre*t2;
+                var ti= rng(0,0.01,2*Math.PI);
+                var vi= rngscn(amp1,fre1,ti);
+                var vo= rngscn(amp2,fre2,ti);
+                var vrl= rngvr(vr/vc2,vo);
+
+                if(cla==1)
+                {
+                    vo= po(vo,amp2);
+                    vrl= rngvr(0*vr/vc2,vi);
+                }
+                else if (cla==4) {
+                  vo= po(vo,-amp2);
+                  vrl= rngvr(0*vr/vc2,vi);
+                }
+                else if (cla==2) {
+                  vo= po(vo,(Number(amp)+Number(vr))/vc2);
+                  vrl= rngvr(vr/vc2,vi);
+                }
+                else if (cla==3) {
+                  vo= po(vo,(Number(amp)-Number(vr))/vc2);
+                  vrl= rngvr(-vr/vc2,vi);
+                }
+                else if (cla==5) {
+                  vo= po(vo,(-Number(amp)+Number(vr))/vc2);
+                  vrl= rngvr(vr/vc2,vi);
+                }
+                else if (cla==6) {
+                  vo= po(vo,(-Number(amp)-Number(vr))/vc2);
+                  vrl= rngvr(-vr/vc2,vi);
+                }
+                var tim1= rng(0,1/(ti.length),t1);
+                var tim2= rng(0,1/(ti.length),t2);
+                var ti1= po(tim1,px1);
+                var ti2= po(tim2,px2);
+                vi= po(vi,py1);
+                vo= po(vo,py2);
+                vrl= po(vrl,py2);
+
+                gr = document.getElementById('grph');
+                    var in1 = {
+                          x: ti1,
+                          y: vi,
+                          type: 'scatter',
+                          name: 'Input(Vi)'
+                    };
+                    var op = {
+                          x: ti2,
+                          y: vo,
+                          type: 'scatter',
+                          name: 'Output(Vo)'
+                    };
+                    var inr = {
+                          x: ti2,
+                          y: vrl,
+                          type: 'line',
+                          name: 'Vr',
+                          line: {
+                            dash: 'dot',
+                            width: 1
+                          }
+                    };
+
+                    if(cha=='1'){
+                      var data = [in1];
+                      var layout={title: 'Input (Vi)',plot_bgcolor: 'black',paper_bgcolor:'black',
+                      font: {
+                        family: 'Courier New, monospace',
+                        size: 18,
+                        color: '#7f7f7f'
+                      },
+                      xaxis: {
+                        title:'Time (s)',
+                        showticklabels: true,
+                        autotick: true},
+                        yaxis: {
+                          title:'Input(V)',
+                          showticklabels: true,
+                          autotick: true}};
+                    }
+                    else if(cha=='2'){
+                      var data = [op,inr];
+                      var layout={title: 'Clamper Circuit Output(Vo)', plot_bgcolor: 'black',paper_bgcolor:'black',
+                      font: {
+                        family: 'Courier New, monospace',
+                        size: 18,
+                        color: '#7f7f7f'
+                      },
+                      xaxis: {
+                        title:'Time (s)',
+                        showticklabels: true,
+                        autotick: true},
+                        yaxis: {
+                          title:'Output (V)',
+                          showticklabels: true,
+                          autotick: true}};
+                      }
+                    else if(cha=='3'){
+                      var data = [in1,op,inr];
+                      var layout={title: 'Clamper Circuit Plot',plot_bgcolor: 'black',paper_bgcolor:'black',text_color:'blue',
+                      font: {
+                        family: 'Courier New, monospace',
+                        size: 18,
+                        color: '#7f7f7f'
+                      },
+                      xaxis: {
+                        title:'Time (s)',
+                        showticklabels: true,
+                        autotick: true},
+                        yaxis: {
+                          title:'Vi/Vo (V)',
+                          showticklabels: true,
+                          autotick: true}};
+                      }
+                      else{
+                        var data = [];
+                        var layout={title: 'Clamper Circuit Plot', plot_bgcolor: 'black',paper_bgcolor:'black',
+                        font: {
+                          family: 'Courier New, monospace',
+                          size: 18,
+                          color: '#7f7f7f'
+                        },
+                        xaxis: {
+                          title:'Time (s)',
+                          showticklabels: true,
+                          autotick: true},
+                          yaxis: {
+                            title:'Input(V)',
+                            showticklabels: true,
+                            autotick: true}};
+                      }
+                    Plotly.newPlot(gr, data,layout);
+  };
+
+  $(document).ready(function(){
+      $("#ctl").slideUp();
+      $(".pic").slideUp();
+      $("#vi").text($("#av").val());
+      $("#f").text($("#fv").val());
+      $("#vc1").text($("#vc1v").val());
+      $("#t1").text($("#t1v").val());
+      $("#px1").text($("#px1v").val());
+      $("#py1").text($("#py1v").val());
+      $("#vc2").text($("#vc2v").val());
+      $("#t2").text($("#t2v").val());
+      $("#px2").text($("#px2v").val());
+      $("#py2").text($("#py2v").val());
+      $("#s1").prop("checked",false);
+      $("#sn").text("Circuit OFF");
+      $("#sn").css("background","red");
+      $("#pa").slideUp();
+      $("#pw").slideUp();
+      $("#I1").slideUp();
+      $("#I2").slideUp();
+      ckt=0;
+      plt(0);
+  });
+
+  $("#c1,#c4").click(function(){
+    $("#I1").slideDown("slow");
+    $("#I2").slideUp();
+    $("#pw").slideDown("slow");
+    $("#v1").slideUp();
+  });
+  $("#c2,#c3,#c5,#c6").click(function(){
+    $("#I1").slideUp("slow");
+    $("#I2").slideDown("slow");
+    $("#v1").slideDown("slow");
+    $("#pw").slideDown("slow");
+    $("#v1").slideDown();
+  });
+  $("#c1").click(function(){
+    cla=1;
+    $(".pic").slideUp();
+    $("#PCa").slideDown("slow");
+  });
+  $("#c2").click(function(){
+    cla=2;
+    $(".pic").slideUp();
+    $("#PCaWPVr").slideDown("slow");
+  });
+  $("#c3").click(function(){
+    cla=3;
+    $(".pic").slideUp();
+    $("#PCaWNVr").slideDown("slow");
+  });
+  $("#c4").click(function(){
+    cla=4;
+    $(".pic").slideUp();
+    $("#NCa").slideDown("slow");
+  });
+  $("#c5").click(function(){
+    cla=5;
+    $(".pic").slideUp();
+    $("#NCaWPVr").slideDown("slow");
+  });
+  $("#c6").click(function(){
+    cla=6;
+    $(".pic").slideUp();
+    $("#NCaWNVr").slideDown("slow");
+  });
+  $("#s1").click(function(){
+    if(ckt==0){
+    $("#sn").text("Circuit ON");
+    $("#sn").css("background","green");
+    $("#ckt").removeClass("col-sm-8");
+    $("#ckt").addClass("col-sm-4");
+    $("#grp").removeClass("col-sm-4");
+    $("#grp").addClass("col-sm-8");
+    $("#ctl").slideDown("slow");
+    $("#grph").slideDown("slow");
+    ckt=1;
+    plt(ch);
+  }
+  else{
+    $("#sn").text("Circuit OFF");
+    $("#sn").css("background","red");
+    $("#grp").removeClass("col-sm-8");
+    $("#grp").addClass("col-sm-4");
+    $("#ckt").removeClass("col-sm-4");
+    $("#ckt").addClass("col-sm-8");
+    $("#ctl").slideUp();
+    $("#grph").slideUp("slow");
+    ckt=0;
+  }
+});
+  $("#c1,#c2,#c3,#c4,#c5,#c6").click(function(){
+    $("#s1").prop("checked",false);
+    $("#sn").text("Circuit OFF");
+    $("#sn").css("background","red");
+    $("#grp").removeClass("col-sm-8");
+    $("#grp").addClass("col-sm-4");
+    $("#ckt").removeClass("col-sm-4");
+    $("#ckt").addClass("col-sm-8");
+    $("#ctl").slideUp();
+    ckt=0;
+    plt(0);
+  });
+      $("#av").change(function(){
+        $("#vi").text($("#av").val());
+        plt(ch);
+      });
+      $("#fv").change(function(){
+        $("#f").text($("#fv").val());
+        plt(ch);
+      });
+      $("#vrv").change(function(){
+        $("#vr").text($("#vrv").val());
+        plt(ch);
+      });
+      $("#vc1v").change(function(){
+        $("#vc1").text($("#vc1v").val());
+        plt(ch);
+      });
+      $("#t1v").change(function(){
+        $("#t1").text($("#t1v").val());
+        plt(ch);
+      });
+      $("#px1v").change(function(){
+        $("#px1").text($("#px1v").val());
+        plt(ch);
+      });
+      $("#py1v").change(function(){
+        $("#py1").text($("#py1v").val());
+        plt(ch);
+      });
+      $("#vc2v").change(function(){
+        $("#vc2").text($("#vc2v").val());
+        plt(ch);
+      });
+      $("#t2v").change(function(){
+        $("#t2").text($("#t2v").val());
+        plt(ch);
+      });
+      $("#px2v").change(function(){
+        $("#px2").text($("#px2v").val());
+        plt(ch);
+      });
+      $("#py2v").change(function(){
+        $("#py2").text($("#py2v").val());
+        plt(ch);
+      });
+      $("#bc1").click(function(){
+        ch=1;
+        plt(ch);
+      });
+      $("#bc2").click(function(){
+        ch=2;
+        plt(ch);
+      });
+      $("#du").click(function(){
+        ch=3;
+        plt(ch);
+      });
